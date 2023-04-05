aca82003e73bf66994dae9b3daa4fae0a81868e5
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-04-05 16:11:47 +0700

    Add function

--- dm_C/Program.cs
+++ dm_C/Program.cs
@@ -10,7 +10,26 @@ namespace dm_C
     {
         static void Main(string[] args)
         {
-
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukkan User ID :");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password :");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan  database tujuan:");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke database:");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    { 
+                     
+                    }
+                }
+            }
         }
     }
 }
