a3b69f79ee8c0a2b669636436568722b80c95a9b
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-04-05 16:21:56 +0700

    menambahkan perintah melihat dan tambah data

--- dm_C/Program.cs
+++ dm_C/Program.cs
@@ -37,7 +37,19 @@ namespace dm_C
                                 conn.Open();
                                 Console.Clear();
                                 while (true)
-                                    
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Melihat Seluruh Data");
+                                        Console.WriteLine("2. Tambah Data");
+                                        Console.WriteLine("3. Keluar");
+                                        Console.Write("\nEnter your choice (1-3): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+                                        switch (ch)
+                                        {
+                                        }
+                                }
                             }
                     }
                 }
