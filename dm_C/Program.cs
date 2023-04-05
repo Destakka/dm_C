8081b081a8ec5ad72f26641ab1aeae527555a683
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-04-05 16:17:08 +0700

    add functionfor  connected from sql to vs

--- dm_C/Program.cs
+++ dm_C/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,8 +26,19 @@ namespace dm_C
                     Console.Write("\nKetik K untuk terhubung ke database:");
                     char chr = Convert.ToChar(Console.ReadLine());
                     switch (chr)
-                    { 
-                     
+                    {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data Source = LAPTOP-8VUKFT0D\\DESTAKKA;" +
+                                    "initial catalog = {0}; " +
+                                    "User ID = {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                    
+                            }
                     }
                 }
             }
